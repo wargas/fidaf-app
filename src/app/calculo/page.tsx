19695c6b697ea3ded273911935b6bf7735fad15f
wargas <teixeira.wargas@gmail.com>
2025-02-07 11:58:39 -0300

    calculo

---
+++ src/app/calculo/page.tsx
@@ -0,0 +1,92 @@
+import Api from "@/libs/api"
+import { sumBy } from "lodash"
+import { Resumo } from "../../.."
+
+const { format } = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: "BRL" })
+
+export default async function PageCalculo({ searchParams }: { searchParams: any }) {
+
+    const mes = parseInt(searchParams.mes || "1")
+
+    const { data } = await Api.get<Resumo[]>('resumo')
+
+    const receitas = data.map(r => ({ ...r, mesInt: parseInt(r.mes.substring(5)) }))
+        .map(r => ({...r, nominal: parseFloat(r.nominal), corrigido: parseFloat(r.corrigido)}))
+        .filter(r => r.mesInt <= mes)
+
+    const anterior = receitas?.filter(r => r.mes.startsWith('2024'))
+    const corrente = receitas?.filter(r => r.mes.startsWith('2025'))
+
+    const anteriorNominal = sumBy(anterior, 'nominal')
+    const correnteNominal = sumBy(corrente, 'nominal')
+    const anteriorCorrigido = sumBy(anterior, 'corrigido')
+    const correnteCorrigido = sumBy(corrente, 'corrigido')
+
+    let incremento = correnteCorrigido - anteriorCorrigido
+
+    if(incremento < 0) {
+        incremento = 0
+    }
+
+    const premioFidaf = incremento * 0.1 * 0.8
+
+    const pontos = 513.9
+
+    const valorAuditor = premioFidaf / pontos * 3
+
+    const valorAnalista = premioFidaf / pontos * 2.1
+
+    return <div className="p-4">
+        <form className="flex gap-2" action="">
+            <select className="select select-bordered" defaultValue={mes} name="mes" >
+                <option value="1">Janeiro</option>
+                <option value="2">Fevereiro</option>
+            </select>
+            <button className="btn" type="submit">Enviar</button>
+        </form>
+
+        <div className="shadow bg-white rounded-box flex py-4 mt-4">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th className="text-right">2024</th>
+                        <th className="text-right">2025</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Receita nominal</td>
+                        <td className="text-right">{format(anteriorNominal)}</td>
+                        <td className="text-right">{format(correnteNominal)}</td>
+                    </tr>
+                    <tr>
+                        <td>Receita corrigida</td>
+                        <td className="text-right">{format(anteriorCorrigido)}</td>
+                        <td className="text-right">{format(correnteCorrigido)}</td>
+                    </tr>
+                    <tr>
+                        <td>Incremento</td>
+                        <td className="text-right" colSpan={2}>{format(incremento)}</td>
+                    </tr>
+                    <tr>
+                        <td>Incremento %</td>
+                        <td className="text-right" colSpan={2}>{(incremento/correnteCorrigido*100).toFixed(2)}%</td>
+                    </tr>
+                    <tr>
+                        <td>Prêmio FIDAF</td>
+                        <td className="text-right" colSpan={2}>{format(premioFidaf)}</td>
+                    </tr>
+                    <tr>
+                        <td>Por auditor</td>
+                        <td className="text-right" colSpan={2}>{format(valorAuditor)}</td>
+                    </tr>
+                    <tr>
+                        <td>Por analista</td>
+                        <td className="text-right" colSpan={2}>{format(valorAnalista)}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Link from "next/link";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,12 +29,14 @@ export default function RootLayout({
       <body data-theme="light"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="h-screen w-full overflow-y-auto">
+        <div className="h-screen bg-gray-50 w-full overflow-y-auto">
 
-          <div className="tabs tabs-boxed">
-            <Link href="/" className="tab">Resumo</Link>
-            <Link href="/receitas" className="tab">Receitas</Link>
-            <Link href="/ipca" className="tab">ipca</Link>
+          <div className="shadow bg-white h-16 flex items-center">
+            <h1 className="px-4 text-xl font-bold">F I D A F</h1>
+            <div className="ml-auto px-4 flex gap-2">
+              <Link href={'/'}>Receitas</Link>
+              <Link href={'/calculo'}>Cálculo</Link>
+            </div>
           </div>
           <div className="w-full max-w-screen-2xl mx-auto">
             {children}
