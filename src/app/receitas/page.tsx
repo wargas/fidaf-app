9c575e5a6f610b8605eca7ada43535a12f1f9eb1
Wargas Teixeira <teixeira.wargas@gmail.com>
2025-02-13 09:57:26 -0300

    align

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -2,7 +2,7 @@ import Api from "@/libs/api";
 import { Recolhimento } from "../../..";
 import { FormReceitas } from "@/components/form-receitas";
 import { format, startOfMonth } from "date-fns";
-import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { filter, groupBy, sumBy } from "lodash";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
@@ -103,60 +103,61 @@ export default async function ReceitasPage({ searchParams }: Props) {
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead className="border-r border-t" rowSpan={2}>Data</TableHead>
-              <TableHead className="border-t border-r" colSpan={2}>ISS</TableHead>
-              <TableHead className="border-t border-r" colSpan={2}>IPTU</TableHead>
-              <TableHead className="border-t border-r" colSpan={2}>ITBI</TableHead>
-              <TableHead className="border-t border-r" colSpan={2}>Juros/Multas</TableHead>
-              <TableHead className="border-t border-r" colSpan={2}>Total</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-t text-center" rowSpan={2}>Data</TableHead>
+              <TableHead className="bg-neutral-50 border-t border-r text-center" colSpan={2}>ISS</TableHead>
+              <TableHead className="bg-neutral-50 border-t border-r text-center" colSpan={2}>IPTU</TableHead>
+              <TableHead className="bg-neutral-50 border-t border-r text-center" colSpan={2}>ITBI</TableHead>
+              <TableHead className="bg-neutral-50 border-t border-r text-center" colSpan={2}>Juros/Multas</TableHead>
+              <TableHead className="bg-neutral-50 border-t border-r text-center" colSpan={2}>Total</TableHead>
             </TableRow>
             <TableRow>
-              <TableHead className="border-r border-b">Nominal</TableHead>
-              <TableHead className="border-r border-b">Corrigido</TableHead>
-              <TableHead className="border-r border-b">Nominal</TableHead>
-              <TableHead className="border-r border-b">Corrigido</TableHead>
-              <TableHead className="border-r border-b">Nominal</TableHead>
-              <TableHead className="border-r border-b">Corrigido</TableHead>
-              <TableHead className="border-r border-b">Nominal</TableHead>
-              <TableHead className="border-r border-b">Corrigido</TableHead>
-              <TableHead className="border-r border-b">Nominal</TableHead>
-              <TableHead className="border-r border-b">Corrigido</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Nominal</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Corrigido</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Nominal</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Corrigido</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Nominal</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Corrigido</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Nominal</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Corrigido</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Nominal</TableHead>
+              <TableHead className="bg-neutral-50 border-r border-b text-center">Corrigido</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {result.filter(d => d.total.nominal != 0).map(item => (
               <TableRow key={item.data}>
-                <TableCell className="border-r">{item.data}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.iss.nominal)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.iss.corrigido)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.iptu.nominal)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.iptu.corrigido)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.itbi.nominal)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.itbi.corrigido)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.acrescimos.nominal)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.acrescimos.corrigido)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.total.nominal)}</TableCell>
-                <TableCell className="border-r">{currencyFormat.format(item.total.corrigido)}</TableCell>
+                <TableHead className="border-r bg-neutral-50">{format(item.data, 'dd/MM/yyyy')}</TableHead>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.iss.nominal)}</TableCell>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.iss.corrigido)}</TableCell>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.iptu.nominal)}</TableCell>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.iptu.corrigido)}</TableCell>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.itbi.nominal)}</TableCell>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.itbi.corrigido)}</TableCell>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.acrescimos.nominal)}</TableCell>
+                <TableCell className="border-r text-right text-xs">{currencyFormat.format(item.acrescimos.corrigido)}</TableCell>
+                <TableCell className="border-r text-right text-xs bg-neutral-50">{currencyFormat.format(item.total.nominal)}</TableCell>
+                <TableCell className="border-r text-right text-xs bg-neutral-50">{currencyFormat.format(item.total.corrigido)}</TableCell>
 
               </TableRow>
             ))}
           </TableBody>
           <TableFooter>
             <TableRow>
-              <TableCell className="border-r">TOTAL</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.iss.nominal)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.iss.corrigido)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.iptu.nominal)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.iptu.corrigido)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.itbi.nominal)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.itbi.corrigido)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.acrescimos.nominal)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.acrescimos.corrigido)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.total.nominal)}</TableCell>
-              <TableCell className="font-bold border-r">{currencyFormat.format(totais.total.corrigido)}</TableCell>
+              <TableHead className="border-r">TOTAL</TableHead>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.iss.nominal)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.iss.corrigido)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.iptu.nominal)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.iptu.corrigido)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.itbi.nominal)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.itbi.corrigido)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.acrescimos.nominal)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.acrescimos.corrigido)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.total.nominal)}</TableCell>
+              <TableCell className="font-bold border-r text-right">{currencyFormat.format(totais.total.corrigido)}</TableCell>
 
             </TableRow>
           </TableFooter>
+          {/* <TableCaption>* valores corrigidos pelo IPCA-E</TableCaption> */}
         </Table>
       </CardContent>
     </Card>
