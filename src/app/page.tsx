10b1f333bc50a5b56aae54b4570c8af221c4cfaa
wargas <teixeira.wargas@gmail.com>
2025-02-13 06:51:31 -0300

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { redirect } from "next/navigation";
 
 export const dynamic = 'force-dynamic'
 
 export default async function Home() {
 
-  
+  redirect('/calculo')
 
   return (
     <div className="mt-4">
