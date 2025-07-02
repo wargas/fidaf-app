14b0c2e44532fd4f9fe0064bcf8af943864817b7
Wargas Teixeira <teixeira.wargas@gmail.com>
2025-07-02 05:57:20 -0300

    charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,128 @@
-import { InputDate } from "@/components/input-date";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { redirect } from "next/navigation";
+"use client"
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ChartConfig, ChartContainer, ChartLegend, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import Api from "@/libs/api";
+import { filter, get, groupBy, sortBy, sumBy } from "lodash";
+import { useEffect, useMemo, useState } from "react";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
+import { Recolhimento } from "../..";
 
 export const dynamic = 'force-dynamic'
 
-export default async function Home() {
+export default function Home() {
 
-  redirect('/calculo')
+  const [data, setData] = useState<any[]>([])
+  const [key, setKey] = useState("total");
+
+  // const data = Array.from({ length: 12 }).map((_, i) => ({ mes: i + 1, iss: Math.random() * 1000, iptu: 500 }))
+
+  const config = {
+    2024: {
+      color: 'var(--chart-1)'
+    },
+    2025: {
+      color: 'var(--chart-2)'
+    }
+  } satisfies ChartConfig
+
+  async function updateData() {
+    const request = await Api.get<Recolhimento>(`recolhimentos`, {
+      params: {
+        inicio: '2024-01-01',
+        fim: '2025-12-31'
+      }
+    })
+
+
+
+    setData(
+      sortBy(
+        Object.values(
+          groupBy(
+            request.data.map(item => {
+              return { ...item, ano: item.data.substring(0, 4), mes: item.data.substring(5, 7) }
+            }),
+            "mes")
+        )
+          .map(itemsOfMonth => {
+            return {
+              mes: itemsOfMonth[0].mes,
+              iss_corrente: sumBy(filter(itemsOfMonth, { "imposto": "ISS", ano: "2025" }), "corrigido"),
+              iptu_corrente: sumBy(filter(itemsOfMonth, { "imposto": "IPTU", ano: "2025" }), "corrigido"),
+              itbi_corrente: sumBy(filter(itemsOfMonth, { "imposto": "ITBI", ano: "2025" }), "corrigido"),
+              acrescimos_corrente: sumBy(filter(itemsOfMonth, { "imposto": "JUROS MULTAS", ano: "2025" }), "corrigido"),
+              total_corrente: sumBy(filter(itemsOfMonth, { ano: "2025" }), "corrigido"),
+              iss_anterior: sumBy(filter(itemsOfMonth, { "imposto": "ISS", ano: "2024" }), "corrigido"),
+              iptu_anterior: sumBy(filter(itemsOfMonth, { "imposto": "IPTU", ano: "2024" }), "corrigido"),
+              itbi_anterior: sumBy(filter(itemsOfMonth, { "imposto": "ITBI", ano: "2024" }), "corrigido"),
+              acrescimos_anterior: sumBy(filter(itemsOfMonth, { "imposto": "JUROS MULTAS", ano: "2024" }), "corrigido"),
+              total_anterior: sumBy(filter(itemsOfMonth, { ano: "2024" }), "corrigido"),
+
+            }
+          }), "mes"
+      )
+    )
+  }
+
+  const selectedData = useMemo(() => {
+    return data.map(item => {
+      return {
+        mes: item.mes,
+        mesString: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"][parseInt(item.mes) - 1],
+        2025: get(item, `${key}_corrente`, 0),
+        2024: get(item, `${key}_anterior`, 0),
+      }
+    })
+  }, [data, key])
+
+  useEffect(() => {
+    updateData()
+  }, [])
 
   return (
-    <div className="mt-4">
+    <div className="mt-4 flex flex-col gap-4">
       <Card>
-        <CardHeader></CardHeader>
-        <CardContent>
-          <div className="w-52">
-            <InputDate value={'2025-11-21'} />
+        <CardHeader>
+          <div className="flex justify-between items-center">
+
+            <div>
+
+              <CardTitle>Arrecadação mensal</CardTitle>
+              <CardDescription>Detalha a Arrecadação durante os 12 meses do ano</CardDescription>
+            </div>
+
+            <Select onValueChange={setKey} value={key}>
+              <SelectTrigger className="w-48">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="total">Total</SelectItem>
+                <SelectItem value="iss">ISS</SelectItem>
+                <SelectItem value="iptu">IPTU</SelectItem>
+                <SelectItem value="itbi">ITBI</SelectItem>
+                <SelectItem value="acrescimos">Juros/Multas</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
+        </CardHeader>
+        <CardContent className="p-0">
+          <ChartContainer config={config} className="max-h-80 w-full" >
+            <BarChart barGap={2} data={selectedData}>
+              <Bar dataKey={`2024`} fill="var(--color-2024)" radius={4} />
+              <Bar dataKey={`2025`} fill="var(--color-2025)" radius={4} />
+              <XAxis dataKey={"mesString"} axisLine={false} tickLine={false} />
+              <YAxis tickFormatter={t => Number(t / 1000 / 1000).toFixed(1) + "M"} type="number" axisLine={false} tickLine={false} />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+              <ChartLegend />
+
+            </BarChart>
+          </ChartContainer>
         </CardContent>
       </Card>
+
+
 
     </div>
   );
--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: process.env.API_URL
+    baseURL: process.env.API_URL || "https://api-fidaf.deltex.com.br"
 })
 
 export default Api;
